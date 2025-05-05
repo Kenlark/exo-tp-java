1af095ccc800ae1081d10a55f2a3365c7ffbce0e
Kenlark <kenzokerachi@hotmail.fr>
2025-05-05 15:45:59 +0200

    feat : ajout de l'exercice du calcul de périmètre

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Entrer le rayon: ");
+    int rayon =  scanner.nextInt();
+
+    double P = 2 * Math.PI * rayon;
+    System.out.println(P);
+
 
     }
 }
