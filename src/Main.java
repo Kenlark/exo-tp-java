560fc64d40072c5a0df4176768789c68f83cdee9
Kenlark <kenzokerachi@hotmail.fr>
2025-05-05 15:51:39 +0200

    feat : ajout de l'exercice du calcul d'age

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+    // Exo Périmètre
 
     Scanner scanner = new Scanner(System.in);
 
-    System.out.print("Entrer le rayon: ");
-    int rayon =  scanner.nextInt();
+    // System.out.print("Entrer le rayon: ");
+    // int rayon =  scanner.nextInt();
 
-    double P = 2 * Math.PI * rayon;
-    System.out.println(P);
+    // double P = 2 * Math.PI * rayon;
+    // System.out.println(P);
+    // -- //
 
+    System.out.print("Entrer votre âge: ");
+    int major = scanner.nextInt();
+
+    if (major < 18) {
+        System.out.println("Vous êtes mineur !");
+    } else {
+        System.out.println("Vous êtes majeur !");
+    }
 
     }
 }
